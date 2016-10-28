b6c102f90460ee8fab80761057e81079346dc2ca
Artiston <artiston@gmail.com>
2016-10-28 10:53:46 -0600

    Add files via upload

---
+++ initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	private static String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value="/greeting",method=RequestMethod.GET)
+	public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World", required = true) String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
